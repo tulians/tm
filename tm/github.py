bd66e4487067507374879ebb24124adee6025cd7
tulians <julianailan@outlook.com>
2017-02-17 23:15:44 -0300

    Added preliminary string sanitization. Modified the Github abstraction scheme.

--- tm/github.py
+++ tm/github.py
@@ -1,38 +1,19 @@
-# Implemented using:
-# --> http://docs.python-requests.org/
-# --> https://developer.github.com/v3/
-# --> https://developer.github.com/v3/repos/contents/
+# tm - Task management.
+# Github abstraction module.
+# Author: Julian Ailan
+# ===================================
 
-import json
-import requests
+# Built-in modules.
+from subprocess import call
+# Project specific modules.
+from utils import sanitize_string
 
-api_url = "https://api.github.com/"
-api_content_url = api_url + "repos/{0}/{1}/contents/{2}"
 
-
-class Github(object):
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.req = requests.get(api_url + "user",
-                                auth=(username, password))
-
-    def get_file(self, repository, path, payload=None):
-        url = api_content_url.format(self.username, repository, path)
-        return requests.get(url, params=payload)
-
-    def create_file(self, repository, path, commit_message, content, file_name,
-                    branch=None, name=None, email=None):
-        payload = {
-            "message": commit_message,
-            "commiter": {
-                "name": name,
-                "email": email
-            },
-            "content": content
-        }
-        url = (api_content_url.format(self.username, repository, path) +
-               file_name)
-        print url
-        return requests.put(url, json=payload, auth=(self.username,
-                                                     self.password))
+def add_files(files):
+    files_to_add = " ".join(files)
+    files_are_safe = sanitize_string(files_to_add)
+    if files_are_safe:
+        git_cmd_string = "git add {}".format(files_to_add)
+        call(git_cmd_string, shell=True)
+    else:
+        print("The given parameters contain not safe characters or commands.")
--- tm/utils.py
+++ tm/utils.py
@@ -5,30 +5,52 @@
 
 """Provides a series of methods to simplify operations."""
 
+import re
+from string import ascii_letters, punctuation
 
-def numberical_list_from_string(string):
+# Generate a set of characters used for string sanitization.
+punctuation_minus = set(punctuation) - set(('.', '_', '-'))
+substrings = set(("rm", "&&", "-f", "null", "/dev/null", "2>&1", "-rf"))
+illegal = [re.escape(item) for item in (punctuation_minus | substrings)]
+illegal_as_str = "|".join(illegal)
+
+
+def numberical_list_from_string(s):
     resulting_list = []
     dont_include = ["[", "]", ","]
-    for char in string:
+    for char in s:
         if char not in dont_include:
             resulting_list.append(int(char))
     return resulting_list
 
 
-def list_from_string(string):
+def list_from_string(s):
     resulting_list = []
     list_parts = ["[", "]"]
     for part in list_parts:
-        string = string.replace(part, "")
-    string = string.replace(" ", "")
-    string = string.split(",")
-    if string == ['']:
+        s = s.replace(part, "")
+    s = s.replace(" ", "")
+    s = s.split(",")
+    if s == ['']:
         return []
-    for substring in string:
+    for substring in s:
         resulting_list.append(substring.replace("'", ""))
     return resulting_list
 
 
-def replace_last_comma(string):
-    list_of_characters = string.rsplit(",", 1)
+def replace_last_comma(s):
+    list_of_characters = s.rsplit(",", 1)
     return " and".join(list_of_characters)
+
+
+# TODO: This solution is O(n.m) where n = len(illegal) and m = len(parts).
+# Look for a more efficient approach.
+def sanitize_string(s):
+    operation = (re.split(illegal_as_str, s))[0]
+    operation = operation.strip()
+    try:
+        operation.decode("utf-8")
+        return operation
+    except UnicodeDecodeError as detail:
+        print("UnicodeDecodeError: " + str(detail))
+        return False
