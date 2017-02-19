21da4161cdfc31bc5ae18ba2e767cbef14184edb
tulians <julianailan@outlook.com>
2017-02-19 16:54:06 -0300

    Added commit and push to github module. Refactored some code in the latter.

--- tm/github.py
+++ tm/github.py
@@ -14,21 +14,45 @@ def add_files(files):
     if not files:
         raise ValueError("No files received for adding.")
     files_to_add = " ".join(files)
-    files_are_safe = sanitize_string(files_to_add)
-    if files_are_safe:
+    if sanitize_string(files_to_add):
         git_cmd_string = "git add {}".format(files_to_add)
-        call(git_cmd_string, shell=True)
+        _execute(git_cmd_string)
     else:
         print("The given parameters contain not safe characters or commands.")
 
 
-def get_status(flags=""):
+def status(flags=""):
     """Performs 'git status [-flags]' operation."""
     flags_to_use = " ".join(flags)
     flags_are_safe = sanitize_string(flags_to_use)
     if flags_are_safe or (flags_are_safe == ""):
         git_cmd_string = "git status {}".format(flags_are_safe)
-        git_cmd_string = git_cmd_string.strip()
-        call(git_cmd_string, shell=True)
+        _execute(git_cmd_string)
     else:
         print("The given parameters contain not safe characters or commands.")
+
+
+def commit(message):
+    """Performs 'git commit -m [-message]' operation."""
+    if message == sanitize_string(message):
+        git_cmd_string = "git commit -m '{}'".format(message)
+        _execute(git_cmd_string)
+    else:
+        print("The given commit message contains not safe characters or"
+              " commands.")
+
+
+def push(server, branch):
+    """Performs 'git push [-server] [-branch]' operation."""
+    squashed = server + branch
+    if sanitize_string(squashed):
+        git_cmd_string = "git push {} {}".format(server, branch)
+        _execute(git_cmd_string)
+    else:
+        print("The given server and/or branch contain not safe characters or"
+              " commands.")
+
+
+def _execute(git_cmd_string):
+    git_cmd_string = git_cmd_string.strip()
+    call(git_cmd_string, shell=True)
