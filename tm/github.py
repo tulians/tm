a50a6891487736022bf0c9c0961e70d8ab2bbc19
tulians <julianailan@outlook.com>
2017-02-25 01:09:05 -0300

    Corrected bug in branch merging.

--- tm/github.py
+++ tm/github.py
@@ -57,14 +57,14 @@ def merge(from_branch, to_branch):
         raise ValueError("No valid branch names received.")
     os.chdir("..")
     git_checkout_string = "git checkout {}".format(to_branch)
-    _execute(git_checkout_string)
+    _execute(git_checkout_string.strip().split(" "))
     git_merge_string = "git merge {}".format(from_branch)
-    _execute(git_merge_string)
+    _execute(git_merge_string.strip().split(" "))
     git_delete_branch_local = "git branch -d {}".format(from_branch)
-    _execute(git_delete_branch_local)
+    _execute(git_delete_branch_local.strip().split(" "))
     git_delete_branch_remote = ("git push origin --delete {}".
                                 format(from_branch))
-    _execute(git_delete_branch_remote)
+    _execute(git_delete_branch_remote.strip().split(" "))
     os.chdir("./tm")
 
 
