24d648350c34a4ed19c6cf9dbf41545cc574b5b5
Julian Ailan <jtulians@gmail.com>
2017-02-26 11:55:59 -0300

    Remove bug in github module.

--- tm/github.py
+++ tm/github.py
@@ -59,10 +59,11 @@ def merge(from_branch, to_branch):
     """Performs the merging of two branches."""
     if not (from_branch and to_branch):
         raise ValueError("No valid branch names received.")
-    os.chdir("..")
-    git_checkout_feat = "git checkout {}".format(from_branch)
-    _execute(git_checkout_feat.strip().split(" "))
-    git_checkout_string = "git fetch && git checkout {}".format(to_branch)
+#    git_checkout_feat = "git checkout {}".format(from_branch)
+#    _execute(git_checkout_feat.strip().split(" "))
+#    git_checkout_string = "git fetch && git checkout {}".format(to_branch)
+#    _execute(git_checkout_string.strip().split(" "))
+    git_checkout_string = "git checkout {}".format(to_branch)
     _execute(git_checkout_string.strip().split(" "))
     git_merge_string = "git merge {}".format(from_branch)
     _execute(git_merge_string.strip().split(" "))
@@ -72,7 +73,6 @@ def merge(from_branch, to_branch):
                                 format(from_branch))
     _execute(git_delete_branch_remote.strip().split(" "))
     push("origin", to_branch)
-    os.chdir("./tm")
     print("Merging successfully completed.")
 
 
